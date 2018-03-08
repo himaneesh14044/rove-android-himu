bcd84e0c4a42267dbc2fd3e29244faae7c04962b
Gursimran Singh <gursimransingh.gsh@gmail.com>
2018-03-08 17:16:50 +0530

    font added.. min api version changed to API26.

---
+++ Rove/app/src/main/java/com/gursimransinghhanspal/rove/activity/Splash.java
@@ -0,0 +1,25 @@
+package com.gursimransinghhanspal.rove.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.activity_splash);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+		// start activity - `LoginViaFacebook`
+		Intent loginViaFacebookActivityIntent = new Intent(this, LoginViaFacebook.class);
+		startActivity(loginViaFacebookActivityIntent);
+		// also call finish() to remove `this` activity from backstack
+		finish();
+	}
+}
--- Rove/app/src/main/java/com/gursimransinghhanspal/rove/controller/DeeplinkController.java
+++ Rove/app/src/main/java/com/gursimransinghhanspal/rove/controller/DeeplinkController.java
@@ -1,13 +1,12 @@
-package com.gursimransinghhanspal.rove;
+package com.gursimransinghhanspal.rove.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
+public class DeeplinkController extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
 	}
 }
