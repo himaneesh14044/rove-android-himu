dd88bd2fa335ff6b4050a011a00a028275ad3844
Gursimran Singh <gursimransingh.gsh@gmail.com>
2018-03-10 02:15:28 +0530

    facebook login working

--- Rove/app/src/main/java/com/gursimransinghhanspal/rove/activity/CredentialLogin.java
+++ Rove/app/src/main/java/com/gursimransinghhanspal/rove/activity/CredentialLogin.java
@@ -10,6 +10,6 @@ public class CredentialLogin extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login_secondary);
+		setContentView(R.layout.activity_credentials_login);
 	}
 }
